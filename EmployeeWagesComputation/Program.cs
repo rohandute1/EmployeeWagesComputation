317af1583cbd024ada65e979e08e5cf3faa9ac5f
Rohan <rohandute1@gmail.com>
2023-05-07 19:43:39 +0530

    [Rohan] Modify Checking Employee Present or not

--- EmployeeWagesComputation/Program.cs
+++ EmployeeWagesComputation/Program.cs
@@ -10,6 +10,19 @@ namespace EmployeeWagesComputation
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
+            Console.WriteLine("Welcome to EmployeeWagesComputation");
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if(IS_PRESENT== randomInput)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
